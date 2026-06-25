cda732b7d4693deadba7c8512a26d2dc5bd974e6
anaclaragl <ag.linhares2@gmail.com>
2026-06-25 11:21:49 -0300

    Flatten repository structure for GitHub Pages deployment and update workflow

--- Portfolio/src/shims-vue.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-import type { DefineComponent } from 'vue'
-
-declare module '*.vue' {
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
